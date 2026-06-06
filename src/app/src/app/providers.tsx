207aa3c4dceb043b1d182799ed147b5140b54554
Ravi9610 <ravindarc594@gmail.com>
2026-06-06 11:04:26 +0530

    Create providers.tsx

---
+++ src/app/src/app/providers.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <BrowserRouter>
+      {children}
+    </BrowserRouter>
+  )
+}
