9b12a5efcc7c72ce94516f9a36a129387f581f03
Ravi9610 <ravindarc594@gmail.com>
2026-06-06 13:25:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import { Providers } from './app/providers'
+import { registerAllApps } from './apps'
 import './styles/globals.css'
 import './styles/theme.css'
 import './styles/windows11.css'
 
+registerAllApps()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
 )
